cf212237225672332b62e1d2adb339fbe757cee1
RJadhav2004 <128611732+RJadhav2004@users.noreply.github.com>
2026-07-07 17:59:58 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "cluster_name" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "bankapp-eks-3"
+  default     = "bankapp-eks"
 }
 
 variable "cluster_version" {
